8a33557c61a1f9f3616ce30f9353e7fc62e418a2
fkenghagho <fkenghag@uni-bremen.de>
2017-08-04 18:06:20 +0200

    :muscle: structure of the repository changed. dialogsystem package and associated package for messages

--- launcher.sh
+++
@@ -1,6 +0,0 @@
-echo "Configuring ros core node ..."
-export ROS_MASTER_URI=http://localhost:11311/
-echo "registring package ..."
-source devel/setup.sh
-echo "Starting package ..."
-roslaunch dialogsystem dialog.launch
