10e1f0470b0a597e448f394d3c7fd2d4801caf5b
fkenghagho <fkenghag@uni-bremen.de>
2017-09-01 14:37:05 +0200

    :rocket: Speech recognizer node improved
    Speech recognizer node improved

--- dialogsystem/nodes/speechRecognizer.py
+++ dialogsystem/nodes/speechRecognizer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import os
 import rospy
 import sys
 from naoqi import (ALBroker, ALProxy, ALModule)
@@ -31,88 +32,96 @@ class SpeechRecognitionWrapper(ALModule):
     """ROS wrapper for Naoqi speech recognition"""
     def __init__(self):
  
-         
-        #constants to mark begin and end of discussions
-        self.TALKSTART=':TALKSTART'
-        self.TALKEND=':TALKEND'
+        try:
+		#constants to mark begin and end of discussions
+		self.TALKSTART=':TALKSTART'
+		self.TALKEND=':TALKEND'
 
-        #start the node
-        rospy.init_node('speechRecognizer')
+		#start the node
+		rospy.init_node('speechRecognizer')
 
-        #parameter reading
-        self.PEPPERIP = rospy.get_param("PEPPERIP", "192.168.101.69")
-        self.PEPPERPORT = int(rospy.get_param("PEPPERPORT", 9559))
-        #self.config=rospy.get_param("CONFIG", "")        
-        # Get a (unique) name for naoqi module which is based on the node name
-        # and is a valid Python identifier (will be useful later)
-        self.naoqi_name = Util.to_naoqi_name( rospy.get_name() )
-        
-        
+		#parameter reading
+		self.PEPPERIP = rospy.get_param("PEPPERIP", "192.168.101.69")
+		self.PEPPERPORT = int(rospy.get_param("PEPPERPORT", 9559))
+		self.PATHTONAOQIPACKAGE =rospy.get_param("PATHTONAOQIPACKAGE", "../NAOqi")
+		self.NAOQIPACKAGENAME =rospy.get_param("NAOQIPACKAGENAME", "suturo16-0.0.0.pkg")
+		self.PATHTOBEHAVIOR =rospy.get_param("PATHTOBEHAVIOR", "behavior_1")
+		self.NAOQIPACKAGEUUID =rospy.get_param("NAOQIPACKAGEUUID", "suturo16")
+		self.naoqi_name = Util.to_naoqi_name( rospy.get_name() )
+		
+		
 
-        #Start ALBroker (needed by ALModule)
-        self.broker = ALBroker(self.naoqi_name + "_broker",
-            "0.0.0.0",   # listen to anyone
-             0,           # find a free port and use it
-            self.PEPPERIP,          # parent broker IP
-            self.PEPPERPORT )       # parent broker port
-        
-        #Init superclass ALModule
-        ALModule.__init__( self, self.naoqi_name )
+		#Start ALBroker (needed by ALModule)
+		self.broker = ALBroker(self.naoqi_name + "_broker",
+		    "0.0.0.0",   # listen to anyone
+		     0,           # find a free port and use it
+		    self.PEPPERIP,          # parent broker IP
+		    self.PEPPERPORT )       # parent broker port
+		
+		#Init superclass ALModule
+		ALModule.__init__( self, self.naoqi_name )
 
-      
-        # Start naoqi proxies
-        self.memory = ALProxy("ALMemory",self.PEPPERIP,self.PEPPERPORT)
-        #self.mic_spk = ALProxy("ALAudioDevice",self.PEPPERIP,self.PEPPERPORT)
-        #self.proxy = ALProxy("ALSpeechRecognition",self.PEPPERIP,self.PEPPERPORT)
-        self.al =ALProxy("ALAutonomousLife",self.PEPPERIP,self.PEPPERPORT)
-        self.al.switchFocus('emptybehavior/behavior_1')
-        self.basic_awareness = ALProxy("ALBasicAwareness", self.PEPPERIP,self.PEPPERPORT)
-        self.motion = ALProxy("ALMotion", self.PEPPERIP,self.PEPPERPORT)
-        
-        #lock speech recognizer
-        self.is_speech_reco_started = False
-        #self.mic_spk.closeAudioInputs()
-        #on stop for publisher
-        rospy.on_shutdown(self.cleanup)
-        #Keep publisher to send word recognized
-        self.pub = rospy.Publisher("~word_recognized", String,queue_size=1000 )
-        # Subscribe to the recognition topic
-        rospy.Subscriber('/rpc_server/recognition', String, self.on_word_recognized);
+	      
+		# Start naoqi proxies
+		self.memory = ALProxy("ALMemory",self.PEPPERIP,self.PEPPERPORT)
+		self.packageManager=self.memory.session().service('PackageManager')
+		#self.packageManager.install(os.path.join(self.PATHTONAOQIPACKAGE,self.NAOQIPACKAGENAME))
+		self.basic_awareness = ALProxy("ALBasicAwareness", self.PEPPERIP,self.PEPPERPORT)
+		self.basic_awareness.setEngagementMode("FullyEngaged")
+		self.basic_awareness.setTrackingMode("MoveContextually")
+		self.basic_awareness.startAwareness()
+		self.motion = ALProxy("ALMotion", self.PEPPERIP,self.PEPPERPORT)
+		self.motion.wakeUp()
+		self.al =ALProxy("ALAutonomousLife",self.PEPPERIP,self.PEPPERPORT)
+		self.al.switchFocus(os.path.join(self.NAOQIPACKAGEUUID,self.PATHTOBEHAVIOR))        
 
 
-        #Install global variables needed by Naoqi
-        self.install_naoqi_globals()
+		#lock speech recognizer
+		self.is_speech_reco_started = False
+		#self.mic_spk.closeAudioInputs()
+		#on stop for publisher
+		rospy.on_shutdown(self.cleanup)
+		#Keep publisher to send word recognized
+		self.pub = rospy.Publisher("~word_recognized", String,queue_size=1000 )
+		# Subscribe to the recognition topic
+		rospy.Subscriber('/rpc_server/recognition', String, self.on_word_recognized);
 
 
-        #loading the vocabulary
-        #rospy.loginfo( 'FILE CONFIG:'+self.config)
-        #rospy.loginfo('Loading vocabulary...')
-        #with open(self.config) as f:
-        #     self.vocabulary= f.readlines()
-        #for i in range(len(self.vocabulary)):
-        #    self.vocabulary[i]=self.vocabulary[i][:-1]
-        
-        #kill running modules  
-        for i in range(len(Constants.EVENT)):
-		#Check no one else is subscribed to this event
-		subscribers = self.memory.getSubscribers(Constants.EVENT[i])
-		if subscribers:
-		    rospy.logwarn(Constants.EVENT[i]+" already in use by another node")
-		    for module in subscribers:
-		        self.stop(module,Constants.EVENT[i])
+		#Install global variables needed by Naoqi
+		self.install_naoqi_globals()
 
 
-        #subscribe to different events
-        self.memory.subscribeToEvent(
-            Constants.EVENT[0],
-            self.naoqi_name,
-            self.on_human_detected.func_name)
+		#loading the vocabulary
+		#rospy.loginfo( 'FILE CONFIG:'+self.config)
+		#rospy.loginfo('Loading vocabulary...')
+		#with open(self.config) as f:
+		#     self.vocabulary= f.readlines()
+		#for i in range(len(self.vocabulary)):
+		#    self.vocabulary[i]=self.vocabulary[i][:-1]
+		
+		#kill running modules  
+		for i in range(len(Constants.EVENT)):
+			#Check no one else is subscribed to this event
+			subscribers = self.memory.getSubscribers(Constants.EVENT[i])
+			if subscribers:
+			    rospy.logwarn(Constants.EVENT[i]+" already in use by another node")
+			    for module in subscribers:
+				self.stop(module,Constants.EVENT[i])
 
-        self.memory.subscribeToEvent(
-             Constants.EVENT[1],
-             self.naoqi_name,
-             self.on_human_lost.func_name)
 
+		#subscribe to different events
+		self.memory.subscribeToEvent(
+		    Constants.EVENT[0],
+		    self.naoqi_name,
+		    self.on_human_detected.func_name)
+
+		self.memory.subscribeToEvent(
+		     Constants.EVENT[1],
+		     self.naoqi_name,
+		     self.on_human_lost.func_name)
+	        rospy.loginfo("Speech recognizer started successfully: ")
+	except Exception,e:
+		      rospy.logwarn("Speech recognizer did not start properly: "+str(e))
 
        
 
@@ -168,7 +177,6 @@ class SpeechRecognitionWrapper(ALModule):
         globals()["memory"] = self.memory
 
     #stop subscribers to a particular events
-    
     def stop(self,module,events):
         rospy.loginfo("Unsubscribing '{}' from NAO services".format(
             module))
@@ -187,11 +195,9 @@ class SpeechRecognitionWrapper(ALModule):
         try:
             self.memory.unsubscribeToEvent( Constants.EVENT[0], module )
             self.memory.unsubscribeToEvent( Constants.EVENT[1], module )
-            #self.memory.unsubscribeToEvent( Constants.EVENT[2], module )
-            #self.proxy.unsubscribe(module)
             self.basic_awareness.stopAwareness()
             #self.motion.rest()
-            self.al.stopFocus('emptybehavior/behavior_1')
+            self.al.stopFocus()
             self.broker.shutdown()
         except RuntimeError:
             rospy.logwarn("Could not unsubscribe from NAO services")
@@ -208,15 +214,8 @@ if __name__=="__main__":
     
       global sr  
       sr=SpeechRecognitionWrapper()
-      #start
-      #sr.motion.wakeUp()
-      sr.basic_awareness.setEngagementMode("FullyEngaged")
-      sr.basic_awareness.setTrackingMode("MoveContextually")
-      sr.basic_awareness.startAwareness()
-      #loop
       rospy.spin()
       sys.exit(0)
-
 
 
 
