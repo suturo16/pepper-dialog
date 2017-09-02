53174484a4516bea5d996f4758c82c077b7d67a8
fkenghagho <fkenghag@uni-bremen.de>
2017-09-02 08:42:09 +0200

    :ring: Installation file updated
    Installation file updated

--- dialogsystem/installer.sh
+++ dialogsystem/installer.sh
@@ -1,16 +1,34 @@
-echo "Installing prerequisites ..."
-cd ./CMU
-sudo apt-get install gcc automake autoconf libtool bison swig python-dev libpulse-dev
-echo "Installing sphinxbase ..."
-mkdir sphinx-source
-cd sphinx-source/
-git clone https://github.com/cmusphinx/sphinxbase.git
-./sphinxbase/autogen.sh
-make
-sudo make install
-# echo "Compiling package ..."
-# catkin build dialogsystem
-# echo "registring package ..."
-# source devel/setup.sh
-echo "changing nodes to executables ..."
-sudo chmod +777 ../../nodes/*.*
+#Variables. Change the default values if needed
+
+tput setaf 7
+echo "Initialiyzing temporary global variables ..."
+tput setaf 2
+export PACKAGE_FOLDER=pepperdialog
+export PACKAGE_SOURCE=src
+export GIT_DIALOG_SYSTEM=https://github.com/suturo16/pepper-dialog
+export GIT_DIALOG_REPO=pepper-dialog
+export DIALOG_PACKAGE_NAME=dialogsystem
+export GIT_CHATSCRIPT=https://github.com/bwilcox-1234/ChatScript
+export PYTHON_NAOQI_TAR_GZ_PATH=$HOME/Downloads/pynaoqi-python2.7-2.5.5.5-linux64.tar.gz
+export GIT_ROS_NAOQI_BRIDGE=https://github.com/ros-naoqi/naoqi_bridge_msgs
+
+
+tput setaf 7
+#install ros-naoqi bridge 
+echo "Installing ros-naoqi bridge ..."
+tput setaf 2
+cd ..
+git clone $GIT_ROS_NAOQI_BRIDGE
+cd ..
+cd ..
+catkin build 
+cd $PACKAGE_SOURCE/$GIT_DIALOG_REPO/$DIALOG_PACKAGE_NAME
+
+tput setaf 7
+#install  
+echo "Installing modified CMU PocketSphinx ..."
+echo "Installing dependencies"
+tput setaf 2
+cd CMU
+sudo apt-get install gcc g++ automake autoconf libtool bison swig python-dev libpulse-dev libboost-dev libxmlrpc-core-c3-dev
+
