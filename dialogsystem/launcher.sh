2a55e3f504a4afc37fe64fffc63d0e5d9d44534c
fkenghagho <fkenghag@uni-bremen.de>
2017-09-06 16:48:28 +0200

    :ring: Launcher updated
    Launcher updated

--- dialogsystem/launcher.sh
+++ dialogsystem/launcher.sh
@@ -1,6 +1,6 @@
 echo "Configuring ros core node ..."
 export ROS_MASTER_URI=http://localhost:11311/
 echo "registring package ..."
-source devel/setup.sh
+source devel/setup.bash
 echo "Starting package ..."
 roslaunch dialogsystem dialog.launch
