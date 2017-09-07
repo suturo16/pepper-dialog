807aa9295e41422f7a232eefd1b7950586d3c808
fkenghagho <fkenghag@uni-bremen.de>
2017-09-07 16:39:04 +0200

    :ring: Launcher updated
    Launcher updated

--- dialogsystem/launcher.sh
+++ dialogsystem/launcher.sh
@@ -1,4 +1,6 @@
 echo "registring package ..."
 source devel/setup.bash
+echo "Localizing OpenCV3 ..."
+export PYTHONPATH=src/pepper-dialog/dialogsystem/OpenCV/local/lib/python2.7/dist-packages:$PYTHONPATH
 echo "Starting package ..."
 roslaunch dialogsystem dialog.launch
